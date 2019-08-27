6be14e6e98431b9a16c17d9c782fb730e2947c22
johndavemanuel <jdmanuel242@gmail.com>
2019-08-27 15:44:36 +0800

    add Roboto font and test Workbox

--- service-worker.js
+++ service-worker.js
@@ -7,32 +7,33 @@ var filesToCache = [
 ];
 
 // Start the service worker and cache all of the app's shell content
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
+    caches.open(staticCacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 // Check if server worker is activated
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   console.log('Service worker has been activate.');
   // Delete old static cache
   e.waitUntil(
-      caches.keys().then(cacheNames => {
-          return Promise.all(cacheNames
-              .filter(cacheName => cacheName !== staticCacheName)
-              .map(cacheName => caches.delete(cacheName))
-          );
-      })
+    caches.keys().then(cacheNames => {
+      console.log(cacheNames);
+      return Promise.all(cacheNames
+        .filter(cacheName => cacheName !== staticCacheName)
+        .map(cacheName => caches.delete(cacheName))
+      );
+    })
   );
 });
 
 // Serve cached content when offline
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
